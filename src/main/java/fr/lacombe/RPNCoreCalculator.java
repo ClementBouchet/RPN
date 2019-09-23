7cc5379fb9efc08240a25c623bf50f49ce6cfdf7
ClementBouchet <bouchetclem@gmail.com>
2019-09-23 11:15:23 +0200

    implement multiplication of two inputs

--- src/main/java/fr/lacombe/RPNCoreCalculator.java
+++ src/main/java/fr/lacombe/RPNCoreCalculator.java
@@ -2,7 +2,10 @@ package fr.lacombe;
 
 public class RPNCoreCalculator {
 
-    public int calculate(int firstInput, int secondInput) {
+    public int calculate(int firstInput, int secondInput, String operator) {
+        if("*".equals(operator)){
+            return firstInput*secondInput;
+        }
         return firstInput+secondInput;
     }
 }
--- src/test/java/RPNCoreCalculatorTest.java
+++ src/test/java/RPNCoreCalculatorTest.java
@@ -9,19 +9,25 @@ public class RPNCoreCalculatorTest {
     @Test
     public void when_add_zero_plus_zero_return_zero() {
         RPNCoreCalculator calculator = new RPNCoreCalculator();
-        assertThat(calculator.calculate(0, 0)).isEqualTo(0);
+        assertThat(calculator.calculate(0, 0, "")).isEqualTo(0);
     }
 
 
     @Test
     public void when_add_input_plus_zero_return_input() {
         RPNCoreCalculator calculator = new RPNCoreCalculator();
-        assertThat(calculator.calculate(3, 0)).isEqualTo(3);
+        assertThat(calculator.calculate(3, 0, "")).isEqualTo(3);
     }
 
     @Test
     public void when_add_2_plus_3_return_5() {
         RPNCoreCalculator calculator = new RPNCoreCalculator();
-        assertThat(calculator.calculate(2,3)).isEqualTo(5);
+        assertThat(calculator.calculate(2,3, "")).isEqualTo(5);
+    }
+
+    @Test
+    public void when_multiply_2_by_3_return_6() {
+        RPNCoreCalculator calculator = new RPNCoreCalculator();
+        assertThat(calculator.calculate(2,3,"*")).isEqualTo(6);
     }
 }
