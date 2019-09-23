c9076c9709a06a9f17e9831eeeb6491f3917c86a
ClementBouchet <bouchetclem@gmail.com>
2019-09-23 17:15:32 +0200

    extract method

--- src/main/java/fr/lacombe/RPNFormatter.java
+++ src/main/java/fr/lacombe/RPNFormatter.java
@@ -10,14 +10,17 @@ public class RPNFormatter {
         String[] inputs = input.split(" ");
         List<RPNInput> inputList = new ArrayList<>();
         for (int i = 0; i < inputs.length; i++) {
-            if("+".equals(inputs[i]) || "*".equals(inputs[i]) || "/".equals(inputs[i]) || "-".equals(inputs[i])){
-                inputList.add(RPNInput.operator(inputs[i]));
-            }else {
-                inputList.add(RPNInput.integer(Integer.valueOf(inputs[i])));
-            }
+            addElementInList(inputs[i], inputList);
         }
-
         return inputList.toArray(new RPNInput[0]);
+    }
+
+    private static void addElementInList(String input, List<RPNInput> inputList) {
+        if("+".equals(input) || "*".equals(input) || "/".equals(input) || "-".equals(input)){
+            inputList.add(RPNInput.operator(input));
+        }else {
+            inputList.add(RPNInput.integer(Integer.valueOf(input)));
+        }
     }
 
     static List<Integer> extractIntegers(RPNInput[] inputs){
